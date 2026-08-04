b96e433f5ab2abb6e3eadce209843f36ddfb9658
nikita <nikitagmyryak@gmail.com>
2026-08-04 12:58:31 +0400

    implemented SavingAccount.java(basic functions)

--- src/domain_models/SavingsAccount.java
+++ src/domain_models/SavingsAccount.java
@@ -1,4 +1,24 @@
 package domain_models;
 
 public class SavingsAccount extends Account {
+
+    private double interestRate;
+
+    public SavingsAccount(String accountNumber, String holderName, double balance, double interestRate) {
+        super(accountNumber, holderName, balance);
+        this.interestRate = interestRate;
+    }
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    public void addInterest(){
+        double interest = interestRate * balance;
+        balance += interest;
+    }
 }
