39f147f35ebabdbd1e4956b8458404fa371b4b19
nikita <nikitagmyryak@gmail.com>
2026-08-04 14:53:03 +0400

    fixed withdraw. now cant withdraw negative amount

--- src/domain_models/Account.java
+++ src/domain_models/Account.java
@@ -37,6 +37,9 @@ public abstract class Account {
     }
 
     public void withdraw(double amount) {
+        if(amount < 0) {
+            throw new InvalidAmountException("Invalid amount. Cannot withdraw negative balance");
+        }
         if (balance >= amount) {
             balance -= amount;
         }
