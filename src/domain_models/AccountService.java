c1ca1701d17c7ab59aa243933e96c832ebffc04b
nikita <nikitagmyryak@gmail.com>
2026-09-09 11:51:39 +0200

    feat: add getAccount method to AccountService

--- src/domain_models/AccountService.java
+++ src/domain_models/AccountService.java
@@ -1,9 +1,17 @@
 package domain_models;
 
+import exceptions.AccountNotFoundException;
+
 public class AccountService {
     private final AccountRepository repository;
 
     public AccountService(AccountRepository repository) {
         this.repository = repository;
+    }
+
+    public Account getAccount(String accountNumber){
+        return repository
+                .findById(accountNumber)
+                .orElseThrow(() -> new AccountNotFoundException("Account was not found"));
     }
 }
