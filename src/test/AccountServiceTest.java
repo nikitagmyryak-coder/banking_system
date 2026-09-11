6bd186512d28fdb546fe58aae57a3d4a468b7ec1
nikita <nikitagmyryak@gmail.com>
2026-09-11 22:17:50 +0200

    test: add unit tests covering deposit, withdraw, transfer, and addInterestRate

--- src/test/AccountServiceTest.java
+++ src/test/AccountServiceTest.java
@@ -1,49 +1,82 @@
 package test;
 
 import domain_models.CheckingAccount;
-
-import static org.junit.Assert.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import domain_models.AccountService;
 import domain_models.InMemoryAccountRepository;
+import domain_models.SavingsAccount;
+import exceptions.AccountNotFoundException;
 import exceptions.InsufficientFundsException;
+import exceptions.InvalidAmountException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class AccountServiceTest {
-    @org.junit.Test
+
+    private InMemoryAccountRepository imr;
+    private AccountService as;
+    private CheckingAccount caTest;
+    private SavingsAccount saTest;
+
+    @BeforeEach
+    public void setUp() {
+        imr = new InMemoryAccountRepository();
+        as = new AccountService(imr);
+        caTest = as.createCheckingAccount("Checking Account Test", 500, 1000);
+        saTest = as.createSavingsAccount("Savings Account", 10, 0.05);
+    }
+
     @Test
     public void depositIncreasesBalanceSuccess() {
-        InMemoryAccountRepository imr = new InMemoryAccountRepository();
-        AccountService as = new AccountService(imr);
-
-        CheckingAccount caTest = as.createCheckingAccount("Checking Account Test", 500, 1000);
-
         caTest.deposit(200);
         assertEquals(700.0, caTest.getBalance(), 0.001);
     }
 
-    @org.junit.Test
     @Test
-    public void withdrawDecreasesBalanceSuccess(){
-        InMemoryAccountRepository imr = new InMemoryAccountRepository();
-        AccountService as = new AccountService(imr);
-
-        CheckingAccount caTest = as.createCheckingAccount("Checking Account Test", 500, 1000);
-
+    public void withdrawDecreasesBalanceSuccess() {
         caTest.withdraw(200);
         assertEquals(300.0, caTest.getBalance(), 0.001);
     }
 
-    @org.junit.Test
     @Test
-    public void withdrawFail(){
-        InMemoryAccountRepository imr = new InMemoryAccountRepository();
-        AccountService as = new AccountService(imr);
-
-        CheckingAccount caTest = as.createCheckingAccount("Checking Account Test", 500, 1000);
-
+    public void withdrawFail() {
         assertThrows(InsufficientFundsException.class, () -> caTest.withdraw(2000));
     }
 
+    @Test
+    public void transferNegativeAmount() {
+        assertThrows(InvalidAmountException.class, () -> as.transfer(
+                caTest.getAccountNumber(), saTest.getAccountNumber(), -1.0));
+    }
 
+    @Test
+    public void depositNegative(){
+        assertThrows(InvalidAmountException.class, () -> as.deposit(caTest.getAccountNumber(), -1));
+    }
+
+    @Test
+    public void transferHappy(){
+        as.transfer(caTest.getAccountNumber(), saTest.getAccountNumber(), 1);
+        assertEquals(11.0, saTest.getBalance(), 0.001);
+    }
+
+    @Test
+    public void getAccountFail(){
+        assertThrows(AccountNotFoundException.class,
+                () -> as.getAccount(""));
+    }
+
+    @Test
+    public void addInterestRateHappy(){
+        as.addInterestRate(saTest.getAccountNumber());
+        assertEquals(10.5, saTest.getBalance(), 0.001);
+    }
+
+    @Test
+    public void addInterestRateFail(){
+        assertThrows(UnsupportedOperationException.class,
+                () -> as.addInterestRate(caTest.getAccountNumber()));
+    }
 }
