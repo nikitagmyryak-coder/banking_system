6fb1ef9ae12a3fde118e47d0446e59d8f9fe3a1b
nikita <nikitagmyryak@gmail.com>
2026-09-15 17:03:25 +0200

    feat: create JdbcTransactionRepository.java and declare final Strings

--- src/Main.java
+++ src/Main.java
@@ -14,11 +14,19 @@ public class Main {
         SavingsAccount s2 = as.createSavingsAccount("Savings Two", 2000, 0.03);
         SavingsAccount s3 = as.createSavingsAccount("Savings Three", 500, 0.07);
 
+        CheckingAccount c4 = as.createCheckingAccount("Checking Four", 1500, 500);
+        SavingsAccount s4 = as.createSavingsAccount("Savings Four", 3000, 0.04);
+
         as.deposit(c1.getAccountNumber(), 100);
         as.withdraw(c2.getAccountNumber(), 50);
         as.transfer(c3.getAccountNumber(), s1.getAccountNumber(), 100);
         as.addInterestRate(s2.getAccountNumber());
 
+
+        as.deposit(c4.getAccountNumber(), 250);
+        as.withdraw(c4.getAccountNumber(), 1000);
+        as.transfer(s4.getAccountNumber(), c4.getAccountNumber(), 500);
+        as.addInterestRate(s4.getAccountNumber());
 
     }
 }
---
+++ src/domain_models/JdbcTransactionRepository.java
@@ -0,0 +1,7 @@
+package domain_models;
+
+public class JdbcTransactionRepository {
+    private final String url = "jdbc:mysql://localhost:3306/banking_system";
+    private final String user = "root";
+    private final String password = "My_Mysql1";
+}
