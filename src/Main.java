437a6bb3998d45ae4421bcc9f96ad704fd3022cd
nikita <nikitagmyryak@gmail.com>
2026-09-17 19:34:22 +0200

    feat: add feat to register an account via window

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,9 @@ import gui.LoginPage;
 public class Main {
     public static void main(String[] args) {
 
-        JdbcAccountRepository jar = new JdbcAccountRepository();
-        JdbcTransactionRepository jtr = new JdbcTransactionRepository();
-        AccountService as = new AccountService(jar,jtr);
+//        JdbcAccountRepository jar = new JdbcAccountRepository();
+//        JdbcTransactionRepository jtr = new JdbcTransactionRepository();
+//        AccountService as = new AccountService(jar,jtr);
 
 
         LoginPage login = new LoginPage();
---
+++ src/gui/SignUpPage.java
@@ -0,0 +1,117 @@
+package gui;
+
+import domain_models.AccountService;
+import domain_models.JdbcAccountRepository;
+import domain_models.JdbcTransactionRepository;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SignUpPage extends JFrame implements ActionListener {
+
+    JLabel welcomeText;
+
+    JLabel nameLabel;
+    JTextField name;
+
+    JLabel passwordLabel;
+    JPasswordField password;
+
+    JLabel passwordValidationLabel;
+    JPasswordField passwordValidation;
+
+    JButton registerButton;
+
+    ButtonGroup group;
+    JRadioButton accountTypeOptionChecking;
+    JRadioButton accountTypeOptionSavings;
+
+
+    JdbcAccountRepository jar = new JdbcAccountRepository();
+    JdbcTransactionRepository jtr = new JdbcTransactionRepository();
+    AccountService as = new AccountService(jar,jtr);
+
+    SignUpPage() {
+        welcomeText = new JLabel("Welcome! Please enter you full name and password");
+        welcomeText.setBounds(50, 30, 400, 30);
+
+        nameLabel = new JLabel("Full Name:");
+        nameLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+        nameLabel.setBounds(40, 95, 120, 30);
+        name = new JTextField();
+        name.setBounds(170, 95, 230, 30);
+
+        passwordLabel = new JLabel("Password:");
+        passwordLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+        passwordLabel.setBounds(40, 150, 120, 30);
+        password = new JPasswordField();
+        password.setBounds(170, 150, 230, 30);
+
+        passwordValidationLabel = new JLabel("Confirm Password:");
+        passwordValidationLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+        passwordValidationLabel.setBounds(40, 205, 120, 30);
+        passwordValidation = new JPasswordField();
+        passwordValidation.setBounds(170, 205, 230, 30);
+
+
+
+        accountTypeOptionChecking = new JRadioButton("Checking");
+        accountTypeOptionChecking.setBounds(170, 280, 100, 30);
+        accountTypeOptionSavings = new JRadioButton("Savings");
+        accountTypeOptionSavings.setBounds(280, 280, 100, 30);
+
+        group = new ButtonGroup();
+        group.add(accountTypeOptionChecking);
+        group.add(accountTypeOptionSavings);
+
+
+
+        registerButton = new JButton("Register");
+        registerButton.setBounds(170, 310, 230, 35);
+        registerButton.addActionListener(this);
+
+        this.setSize(500, 450);
+        this.setTitle("Sign Up Page");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.setLayout(null);
+        this.setVisible(true);
+        this.add(welcomeText);
+        this.add(nameLabel);
+        this.add(name);
+        this.add(passwordLabel);
+        this.add(password);
+        this.add(passwordValidationLabel);
+        this.add(passwordValidation);
+        this.add(registerButton);
+        this.add(accountTypeOptionChecking);
+        this.add(accountTypeOptionSavings);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        if(e.getSource() == registerButton) {
+            String fullNameString = name.getText();
+
+            char[] passwordChars = password.getPassword();
+            char[] validationChars = passwordValidation.getPassword();
+
+            if (!java.util.Arrays.equals(passwordChars, validationChars)) {
+                JOptionPane.showMessageDialog(this, "Passwords do not match!", "Error", JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+            String pswrd = String.valueOf(passwordChars);
+
+            if (accountTypeOptionChecking.isSelected()) {
+                as.createCheckingAccount(fullNameString, pswrd, 100.0);
+                JOptionPane.showMessageDialog(this, "Checking Account created successfully!");
+            } else {
+                as.createSavingsAccount(fullNameString, pswrd, 0.05);
+                JOptionPane.showMessageDialog(this, "Savings Account created successfully!");
+            }
+        }
+        this.dispose();
+    }
+}
