0c39a3962b99da4a961ffbbcc745e2fe5f7d4f5d
nikita <nikitagmyryak@gmail.com>
2026-09-17 16:56:39 +0200

    style: add login and password text fields; enter and sign up buttons, no actual use for now

--- src/gui/MyFrame.java
+++ src/gui/MyFrame.java
@@ -1,33 +1,48 @@
 package gui;
 
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class MyFrame extends JFrame implements ActionListener {
     JButton enter;
+    JButton signUp;
+    JLabel label;
+    JTextField login;
+    JPasswordField password;
 
     public MyFrame(){
 
-        JLabel login = new JLabel();
-        login.setText("Login");
-        login.setHorizontalAlignment(SwingConstants.CENTER);
-        login.setVerticalAlignment(SwingConstants.CENTER);
-        login.setBounds(200, 40, 80, 10);
+        label = new JLabel();
+        label.setText("Login");
+        label.setHorizontalAlignment(SwingConstants.CENTER);
+        label.setVerticalAlignment(SwingConstants.CENTER);
+        label.setBounds(150, 60, 200, 30);
 
         enter = new JButton("Enter");
-        enter.setBounds(200, 100, 100, 50);
+        enter.setBounds(150, 205, 200, 35);
         enter.addActionListener(this);
 
+        signUp = new JButton("Sigh Up");
+        signUp.setBounds(150, 250, 200, 35);
+        signUp.addActionListener(this);
+
+        login = new JTextField();
+        login.setBounds(150, 90, 200, 35);
+
+        password = new JPasswordField();
+        password.setBounds(150, 145, 200, 35);
 
         this.setSize(500, 500);
         this.setTitle("Banking System");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
         this.setLayout(null);
-        this.add(login);
+        this.add(label);
         this.add(enter);
+        this.add(login);
+        this.add(password);
+        this.add(signUp);
 
         this.setVisible(true);
     }
@@ -35,7 +50,9 @@ public class MyFrame extends JFrame implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         if(e.getSource() == enter){
-            System.out.println("popa");
+            System.out.println("Enter clicked");
+        } else if (e.getSource() == signUp) {
+            System.out.println("Sign Up clicked");
         }
     }
 }
