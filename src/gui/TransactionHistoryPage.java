1c8b3e83ad7e767842f687203341b3b1f13adcbc
nikita <nikitagmyryak@gmail.com>
2026-09-21 13:26:25 +0200

    feat: add TransactionHistoryPage showing account transaction list(but empty for now)

---
+++ src/gui/TransactionHistoryPage.java
@@ -0,0 +1,49 @@
+package gui;
+
+import domain_models.Account;
+import domain_models.Transaction;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+
+public class TransactionHistoryPage extends JFrame implements ActionListener{
+    Account account;
+    JLabel tableName;
+    JButton back;
+    JList<Transaction> transactionList;
+    JScrollPane scrollPane;
+
+    TransactionHistoryPage(Account account){
+        this.account = account;
+        this.transactionList = new JList<>(account.getTransactionHistory().toArray(new Transaction[0]));
+        this.scrollPane = new JScrollPane(transactionList);
+
+        scrollPane.setBounds(40, 60, 400, 280);
+        this.add(scrollPane);
+
+        tableName = new JLabel("Your transaction history");
+        tableName.setBounds(40, 20, 300, 30);
+        this.add(tableName);
+
+        back = new JButton("Back");
+        back.setBounds(40, 370, 100, 30);
+        back.addActionListener(this);
+        this.add(back);
+
+        this.setSize(500, 450);
+        this.setTitle("Transaction History");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.setLayout(null);
+        this.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == back){
+            this.dispose();
+        }
+    }
+}
